52172b1fe2ca725504f368c72cc92c46e057c1eb
Keesari Vigneshwar Reddy <vigneshwar472@gmail.com>
2025-08-27 18:00:16 +0530

    final commit

--- segmentation_models_pytorch_3d/utils/__init__.py
+++ segmentation_models_pytorch_3d/utils/__init__.py
@@ -1,9 +1,5 @@
 import warnings
 
-from . import train
-from . import losses
-from . import metrics
-
 warnings.warn(
     "`smp.utils` module is deprecated and will be removed in future releases.",
     DeprecationWarning,
